ab2708a1d514373d6760fa6e74ac7545b4ff8ec0
Harris Snyder <harris.snyder@gmail.com>
2021-03-23 16:41:20 -0400

    fix for obvious bug in prev commit

--- skcuda/fft.py
+++ skcuda/fft.py
@@ -17,7 +17,7 @@ from .cufft import CUFFT_COMPATIBILITY_NATIVE, \
      CUFFT_COMPATIBILITY_FFTW_ASYMMETRIC, \
      CUFFT_COMPATIBILITY_FFTW_ALL
 from . import cudart
-from . import misc2
+from . import misc2 as misc
 
 class Plan:
     """
